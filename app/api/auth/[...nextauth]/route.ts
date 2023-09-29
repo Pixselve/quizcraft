382f4d32dc72488c87ab62f3d40d1d119ea5d8a7
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-09-29 15:10:37 +0200

    feat: refresh token

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions, TokenSet } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const scopes = [
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/drive.file",
+];
 export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
@@ -8,29 +13,66 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
       authorization: {
         params: {
-          scope:
-            "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/forms",
+          scope: scopes.join(" "),
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
         },
-        url: "https://accounts.google.com/o/oauth2/auth?prompt=consent&access_type=offline&response_type=code",
       },
     }),
   ],
   session: {
     strategy: "jwt",
   },
-  jwt: {
-    maxAge: 24 * 60 * 60,
-  },
   callbacks: {
-    async jwt({ account, token }) {
+    async jwt({ account, token, user }) {
       if (account) {
-        token.accessToken = account.access_token;
+        return {
+          access_token: account.access_token as string,
+          expires_at: account.expires_at as number,
+          refresh_token: account.refresh_token as string,
+        };
+      } else if (Date.now() / 1000 < token.expires_at) {
+        // If the access token has not expired yet, return it
+        return token;
+      } else {
+        // If the access token has expired, try to refresh it
+        try {
+          // https://accounts.google.com/.well-known/openid-configuration
+          // We need the `token_endpoint`.
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+              client_id: process.env.GOOGLE_CLIENT_ID ?? "",
+              client_secret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token,
+            }),
+            method: "POST",
+          });
+
+          const tokens: TokenSet = await response.json();
+
+          if (!response.ok) throw tokens;
+
+          return {
+            ...token, // Keep the previous token properties
+            access_token: tokens.access_token as string,
+            expires_at: tokens.expires_at as number,
+            // Fall back to the old refresh token, but note that
+            // many providers may only allow using a refresh token once.
+            refresh_token:
+              tokens.refresh_token ?? (token.refresh_token as string),
+          };
+        } catch (e) {
+          console.error("Error refreshing access token", e);
+          // The error property will be used client-side to handle the refresh token error
+          return { ...token, error: "RefreshAccessTokenError" as const };
+        }
       }
-      return token;
     },
     async session({ session, token }) {
-      // @ts-ignore
-      session.accessToken = token.accessToken;
+      session.accessToken = token.access_token;
       return session;
     },
   },
---
+++ types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    error?: "RefreshAccessTokenError"
+    accessToken: string
+  }
+}
+
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    access_token: string
+    expires_at: number
+    refresh_token: string
+    error?: "RefreshAccessTokenError"
+  }
+}
